808905088e7cee9c8d2573d48f474693aa6497f1
Bramheti Patil <bramheti.211433201@vcet.edu.in>
2023-09-10 09:32:31 +0530

    Final Commit

--- app/src/main/java/com/example/cryptocurrencyanalizer/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/cryptocurrencyanalizer/fragment/HomeFragment.kt
@@ -1,60 +1,117 @@
 package com.example.cryptocurrencyanalizer.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.example.cryptocurrencyanalizer.R
+import com.example.cryptocurrencyanalizer.adapter.TopLossGainPagerAdapter
+import com.example.cryptocurrencyanalizer.adapter.TopMarketAdapter
+import com.example.cryptocurrencyanalizer.apis.ApiInterface
+import com.example.cryptocurrencyanalizer.apis.ApiUtilities
+import com.example.cryptocurrencyanalizer.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        getTopCurrencylist()
+
+        setTabLayout()
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun setTabLayout() {
+        val adapter = TopLossGainPagerAdapter(this)
+        binding.contentViewPager.adapter = adapter
+
+        binding.contentViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if(position == 0){
+                    binding.topGainIndicator.visibility = VISIBLE
+                    binding.topLoseIndicator.visibility = GONE
+                }else{
+                    binding.topGainIndicator.visibility = GONE
+                    binding.topLoseIndicator.visibility = VISIBLE
                 }
             }
+        })
+
+        TabLayoutMediator(binding.tabLayout, binding.contentViewPager){
+            tab, position ->
+            var title = if(position == 0){
+                "Top Gainers"
+            }else{
+                "Top Losers"
+            }
+            tab.text = title
+        }.attach()
+    }
+
+
+    private fun getTopCurrencylist() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            val res = ApiUtilities.getInstance().create(ApiInterface::class.java).getMarketData()
+
+            withContext(Dispatchers.Main)
+            {
+                binding.topCurrencyRecyclerView.adapter =
+                    TopMarketAdapter(requireContext(), res.body()!!.data.cryptoCurrencyList)
+            }
+
+            Log.d("SHUBH", "getTopCurrencyList: ${res.body()!!.data.cryptoCurrencyList}")
+        }
     }
 }
+
+
+
+/*  private fun getTopCurrencylist() {
+      lifecycleScope.launch(Dispatchers.IO) {
+          try {
+              val apiInterface = ApiUtilities.getInstance().create(ApiInterface::class.java)
+
+              withContext()
+
+              val response = apiInterface.getMarketData()
+
+              if (response.isSuccessful) {
+                  val marketData = response.body()
+                  if (marketData != null) {
+                      // Now you can work with the marketData
+                      Log.d("SHUBH", "getTopCurrencyList: ${marketData.data.cryptoCurrencyList}")
+                  } else {
+                      Log.e("SHUBH", "Response body is null.")
+                  }
+              } else {
+                  Log.e("SHUBH", "API call failed with code: ${response.code()}")
+              }
+          } catch (e: Exception) {
+              Log.e("SHUBH", "Error: ${e.message}", e)
+          }
+      }
+  }
+
+}*/
