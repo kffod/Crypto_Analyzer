47c49ebde0ecf878a249311fe1e04883cad3c06c
Bramheti Patil <bramheti.211433201@vcet.edu.in>
2023-10-02 22:49:25 +0530

    Final Commit

--- app/src/main/java/com/example/cryptocurrencyanalizer/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyanalizer/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.cryptocurrencyanalizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.PopupMenu
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.cryptocurrencyanalizer.databinding.ActivityMainBinding
 
@@ -18,8 +23,17 @@ class MainActivity : AppCompatActivity() {
         val navNavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
         val navController = navNavHostFragment!!.findNavController()
 
+
         val popupMenu = PopupMenu(this,null)
         popupMenu.inflate(R.menu.bottom_nav_menu)
         binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+
+
     }
+
+
+
 }
+
+
+
